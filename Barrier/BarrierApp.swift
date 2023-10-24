8a59f3cfdc49e8e3243883aabad045586765e2c0
jaredcassoutt <jaredcassoutt@gmail.com>
2023-10-24 11:41:57 -0700

    Initial Commit

---
+++ Barrier/BarrierApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarrierApp.swift
+//  Barrier
+//
+//  Created by Jared Cassoutt on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct BarrierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barrier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Barrier
+//
+//  Created by Jared Cassoutt on 10/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
